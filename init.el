6ec1e8d3c3a2782377d179a22b55a96711439497
Nishant Maniam <nmmaniam@amazon.com>
2022-06-14 14:55:08 -0700

    [Emacs Config V2] 2022 refresh!
    Straight! Vertico Consult Orderless! Tree-Sitter and Eglot!
    Modern Emacs is here!

--- init.el
+++ init.el
@@ -1,37 +1,12 @@
 ;;; init.el --- Emacs init file
 ;;  Author: Nishant Maniam
-;;; Commentary:
-;;; A lightweight Emacs config containing only the essentials: shipped with a custom theme!
-;;; Code:
+
 (defvar file-name-handler-alist-original file-name-handler-alist)
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(setq package-enable-at-startup nil)
 
-;; (setq gc-cons-threshold most-positive-fixnum
-;;       gc-cons-percentage 0.6
-;;       file-name-handler-alist nil
-;;       site-run-file nil)
 
-;; (defvar nish/gc-cons-threshold 20000000)
-
-;; (add-hook 'emacs-startup-hook ; hook run after loading init files
-;;           (lambda ()
-;;             (setq gc-cons-threshold nish/gc-cons-threshold
-;;                   gc-cons-percentage 0.1
-;;                   file-name-handler-alist file-name-handler-alist-original)))
-
-;; (add-hook 'minibuffer-setup-hook (lambda ()
-;;                                    (setq gc-cons-threshold (* nish/gc-cons-threshold 2))))
-;; (add-hook 'minibuffer-exit-hook (lambda ()
-;;                                   (garbage-collect)
-;;                                   (setq gc-cons-threshold nish/gc-cons-threshold)))
-
-;; (require 'package)
-;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-;; (setq package-enable-at-startup nil)
-;; (package-initialize)
-
-;; straight.el https://github.com/raxod502/straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -45,19 +20,37 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; workaround bug in Emacs 26.2
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(straight-use-package 'use-package)
 
-;; (unless (package-installed-p 'use-package)
-;;   (straight-use-package 'use-package))
-
-;; (setq straight-use-package-by-default t)
-;; (setq straight-built-in-pseudo-packages t)
-
+(setq default-directory "~/")
 
 ;; Load main config file "./config.org"
-(require 'org)
+(straight-use-package 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
+ 
 (provide 'init)
-;;; init.el ends here
+;; init.el ends here
+
+
+
+
+
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2ce76d65a813fae8cfee5c207f46f2a256bac69dacbb096051a7a8651aa252b0" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "6128465c3d56c2630732d98a3d1c2438c76a2f296f3c795ebda534d62bb8a0e3" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "5a00018936fa1df1cd9d54bee02c8a64eafac941453ab48394e2ec2c498b834a" "afffd2e82639b878a7e25d153e7e1d47704797d266f5f0c16de427255c918059" default))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
