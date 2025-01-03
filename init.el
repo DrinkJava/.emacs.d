6f2e45b4ef239e558002e7201638978168d3af47
Nishant Maniam <nishant.maniam@gmail.com>
2025-01-03 08:28:16 -0800

    Update init.el to v4

--- init.el
+++ init.el
@@ -2,19 +2,22 @@
 ;;  Author: Nishant Maniam
 
 (defvar file-name-handler-alist-original file-name-handler-alist)
-(setq gc-cons-threshold 100000000)
+(setq gc-cons-threshold 1000000)
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 (setq package-enable-at-startup nil)
 
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
@@ -26,18 +29,12 @@
 
 ;; Load main config file "./config.org"
 (straight-use-package 'org)
+(setq org-agenda-files '("~/nish-roam/daily/" "~/nish-roam/"))
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
  
 (provide 'init)
 ;; init.el ends here
-
-
-
-
-
-
-
-
 
 
 (custom-set-variables
@@ -46,8 +43,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("06ed754b259cb54c30c658502f843937ff19f8b53597ac28577ec33bb084fa52" "249e100de137f516d56bcf2e98c1e3f9e1e8a6dce50726c974fa6838fbfcec6b" "e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" "e266d44fa3b75406394b979a3addc9b7f202348099cfde69e74ee6432f781336" "9cd57dd6d61cdf4f6aef3102c4cc2cfc04f5884d4f40b2c90a866c9b6267f2b3" "d516f1e3e5504c26b1123caa311476dc66d26d379539d12f9f4ed51f10629df3" "11cc65061e0a5410d6489af42f1d0f0478dbd181a9660f81a692ddc5f948bf34" "2ce76d65a813fae8cfee5c207f46f2a256bac69dacbb096051a7a8651aa252b0" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "6128465c3d56c2630732d98a3d1c2438c76a2f296f3c795ebda534d62bb8a0e3" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "5a00018936fa1df1cd9d54bee02c8a64eafac941453ab48394e2ec2c498b834a" "afffd2e82639b878a7e25d153e7e1d47704797d266f5f0c16de427255c918059" default))
- '(warning-suppress-types '((comp))))
+   '("f3781be0be23cc71c89b317489e07a4ad3e885f84c0a618692b53bbd69e60843" "1896e14316f5f9bb073e0cd43fa56993a15b751c02d771114f595b7b18c4ba04" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "6128465c3d56c2630732d98a3d1c2438c76a2f296f3c795ebda534d62bb8a0e3" "9cd57dd6d61cdf4f6aef3102c4cc2cfc04f5884d4f40b2c90a866c9b6267f2b3" "0170347031e5dfa93813765bc4ef9269a5e357c0be01febfa3ae5e5fcb351f09" "b95f61aa5f8a54d494a219fcde9049e23e3396459a224631e1719effcb981dbd" "74e2ed63173b47d6dc9a82a9a8a6a9048d89760df18bc7033c5f91ff4d083e37" default))
+ '(org-confirm-babel-evaluate nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
